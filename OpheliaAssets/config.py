1c07ef40e41316ef952b25a01e4ff8cdc77dbb33
OperavonderVollmer <159807320+OperavonderVollmer@users.noreply.github.com>
2025-02-18 11:52:07 +0800

    Delete OpheliaAssets/config.py

--- OpheliaAssets/config.py
+++
@@ -1,5 +0,0 @@
-debugMode = False
-micIndex = 15               # run getSoundDevices.py in functions folder and get the index
-speakerIndex = 19           # run getSoundDevices.py in functions folder and get the index
-city = "Taguig"
-deepDebugMode = False       # shows particularly annoying debug messages that I'd rather not have on
